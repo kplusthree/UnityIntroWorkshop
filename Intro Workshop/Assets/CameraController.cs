f49011f4605ca87dcb577ab7b53ffadc367cfbf6
Kortney Menefee <kplusmenefee@gmail.com>
2021-11-13 12:01:01 -0500

    Full Workshop

---
+++ Intro Workshop/Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    [HideInInspector]
+    public Vector3 offset;
+
+    void Update()
+    {
+        offset = new Vector3(0, 2.5f, -10);
+
+        // Camera follows the player with specified offset position
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
---
+++ Intro Workshop/Assets/CharacterController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [HideInInspector]
+    public Rigidbody2D rb2d;
+
+    public int speed;
+    [HideInInspector]
+    public bool jump;
+
+    public SpriteRenderer chestRenderer;
+    public Sprite newChest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        jump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (jump == false)
+            {
+                StartCoroutine(Jump());
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * speed;
+        }
+    }
+
+    void ChangeSprite()
+    {
+        chestRenderer.sprite = newChest;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "chest")
+        {
+            ChangeSprite();
+        }
+    }
+
+    IEnumerator Jump()
+    {
+        jump = true;
+
+        for (int i = 0; i < 10; i++)
+        {
+            rb2d.AddForce((Vector2.up * 20));
+        }
+
+        yield return new WaitForSeconds(1);
+
+        jump = false;
+
+        yield return null;
+    }
+}
